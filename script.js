e6f9115bc9b338a9a314b3da3f19096f79dcefee
GIOVANNA SOARES XAVIER <24122285@CFP127ALUNO.LOCAL>
2025-03-20 17:11:17 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
     // criação da tela inicial
     const app = document.getElementById('app')
 
@@ -190,4 +190,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // exibe o botão listar Todos na tela inicial
     listAllButton.style.display = 'block'
-});
+// });
